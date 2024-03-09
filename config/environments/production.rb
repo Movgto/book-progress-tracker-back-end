9f6a731907da89d7762cbc2831c15285ff63fde7
Gumaro Monroy <maromvz@gmail.com>
2024-03-08 23:28:01 -0600

    Remove unnecesary config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-
-  config.public_file_server.enable = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
